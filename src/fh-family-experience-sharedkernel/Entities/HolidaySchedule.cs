636c47fdcc403b0c3775abe4a9a57b7e59646209
MikeRazorMiles <michael.miles@digital.education.gov.uk>
2022-08-31 08:16:08 +0100

    Addition of  key service entities

---
+++ src/fh-family-experience-sharedkernel/Entities/HolidaySchedule.cs
@@ -0,0 +1,12 @@
+﻿namespace fh_family_experience_sharedkernel.Entities;
+
+public class HolidaySchedule :EntityBase
+{
+    public ServiceItem? ServiceItemId { get; set; } = null!;
+    public ServiceAtLocation? ServiceAtLocationId { get; set; } = null!;
+    public Boolean? Closed { get; set; } = null!;
+    public DateTime? OpensAt { get; set; } = null!;
+    public DateTime? ClosesAt { get; set; } = null!;
+    public DateTime? SatrtDate{ get; set; } = null!;
+    public DateTime? EndDate { get; set; } = null!;
+}
