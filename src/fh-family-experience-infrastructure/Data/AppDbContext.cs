6f3d6b3edae8fa530682fd57f65a72e10e253399
MikeRazorMiles <michael.miles@digital.education.gov.uk>
2022-09-14 08:10:09 +0100

    Update DB context

--- src/fh-family-experience-infrastructure/Data/AppDbContext.cs
+++ src/fh-family-experience-infrastructure/Data/AppDbContext.cs
@@ -15,15 +15,17 @@ public class AppDbContext : DbContext
     public DbSet<Eligibility>? Eligibilities => Set<Eligibility>();
     public DbSet<Funding>? Fundings => Set<Funding>();
     public DbSet<HolidaySchedule> HolidaySchedule => Set<HolidaySchedule>();
+    public DbSet<Language> Languages => Set<Language>();
+    public DbSet<LinkTaxonomy> LinkTaxonomies => Set<LinkTaxonomy>();
     public DbSet<Location> Locations => Set<Location>();
     public DbSet<Organisation> Organisations => Set<Organisation>();
     public DbSet<Phone> Phones => Set<Phone>();
     public DbSet<PhysicalAddress> PhysicalAddress => Set<PhysicalAddress>();
     public DbSet<RegularSchedule> RegularSchedules => Set<RegularSchedule>();
     public DbSet<Review> Reviews => Set<Review>();
+    public DbSet<Service> Services => Set<Service>();
     public DbSet<ServiceArea> ServiceAreas => Set<ServiceArea>();
     public DbSet<ServiceAtLocation> ServiceAtLocations => Set<ServiceAtLocation>();
-    public DbSet<Service> Services => Set<Service>();
     public DbSet<ServiceTaxonomy> ServiceTaxonomies => Set<ServiceTaxonomy>();
     public DbSet<Taxononmy> Taxononmies => Set<Taxononmy>();
 }
